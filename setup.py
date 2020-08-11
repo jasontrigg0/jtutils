91d51e7df35241f6cf72ee96eeb9ee2c8cd6a002
Jason Trigg <jasontrigg0@gmail.com>
2020-08-10 22:33:34 -0400

    pypi version 0.0.7

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
     name = 'jtutils',
     packages = ['jtutils'],
-    version = "0.0.6",
+    version = "0.0.7",
     description = 'Placeholder description',
     author = "Jason Trigg",
     author_email = "jasontrigg0@gmail.com",
     url = "https://github.com/jasontrigg0/jtutils",
-    download_url = 'https://github.com/jasontrigg0/jtutils/tarball/0.0.6',
+    download_url = 'https://github.com/jasontrigg0/jtutils/tarball/0.0.7',
     scripts=[],
     install_requires=["requests",
                       "beautifulsoup4"
